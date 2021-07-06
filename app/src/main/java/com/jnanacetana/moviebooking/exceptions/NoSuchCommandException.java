47ed22e694039217eae28b2b549303dcd6b7d32e
Yakshit Jain <yakshit@criodo.com>
2021-07-06 21:23:49 +0530

    Module-6

---
+++ app/src/main/java/com/jnanacetana/moviebooking/exceptions/NoSuchCommandException.java
@@ -0,0 +1,9 @@
+package com.jnanacetana.moviebooking.exceptions;
+
+public class NoSuchCommandException extends Exception{
+
+    @Override
+    public String toString() {
+        return "No such Command Found!";
+    }
+}
---
+++ app/src/main/java/com/jnanacetana/moviebooking/exceptions/NoSuchDataException.java
@@ -0,0 +1,8 @@
+package com.jnanacetana.moviebooking.exceptions;
+
+public class NoSuchDataException extends Exception {
+    @Override
+    public String toString() {
+        return "No such Data Found!";
+    }
+}
